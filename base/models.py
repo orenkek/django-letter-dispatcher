106043478f434fa48fb99f9011fea6b80ae1f21e
anuarcheck <anuarcheck@gmail.com>
2022-03-17 12:20:01 +0500

    feat: Добавил поле имени пользователя в модель

--- base/models.py
+++ base/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 
 class MailList(models.Model):
+    name = models.CharField(max_length=90, null=True)
     email = models.EmailField()
 
     def __str__(self):
