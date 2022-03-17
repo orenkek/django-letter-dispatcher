18234413bd5da325d8a5bf47807d1b2ab2ef0d12
anuarcheck <anuarcheck@gmail.com>
2022-03-17 17:09:14 +0500

    feat: Добавил новое view для вывода пользователей

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from base.views import Index
+from base.views import Index, MailListView
 
 urlpatterns = [
    path('', Index.as_view(), name='index'),
+   path('mail-list/', MailListView.as_view(), name='mail_list'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import View, ListView
+from .models import MailList
 
 
 # Create your views here.
 class Index(View):
     def get(self, request):
         return render(request, 'base/index.html', {})
+
+
+class MailListView(ListView):
+    template_name = 'base/user-list.html'
+    model = MailList
