458216901ad8a1e1c0c680d97a0022edd10891ec
anuarcheck <anuarcheck@gmail.com>
2022-03-17 12:16:20 +0500

    feat: Добавил модель в админку Django

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from base.models import MailList
+
+
 # Register your models here.
+class MailListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', ]
+    list_display_links = ['email', ]
+    search_fields = ['email', ]
+
+
+admin.site.register(MailList, MailListAdmin)
