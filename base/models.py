a47e0adcde667278a7aab766917272f77d0b0612
anuarcheck <anuarcheck@gmail.com>
2022-03-18 11:55:36 +0500

    fix: Подправил поле модели MailList, чтобы не было повторении с почтовыми адресами

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class MailList(models.Model):
     name = models.CharField(max_length=90, null=True)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.email
